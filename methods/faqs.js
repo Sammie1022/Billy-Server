5128c760c40f5d57705f712f416d2b5e17c31e28
Abigail Capio <sammarco1022@gmail.com>
2022-03-03 15:10:55 +0800

    Modify faqs

--- methods/faqs.js
+++ methods/faqs.js
@@ -1,24 +1,16 @@
 import Faqs from '../models/faqs.js'
 import User from '../models/user.js'
+
 const getQuestionsFromOffice = async (req, res) => {
   const { officeName } = req.body
 
-  const result = Faqs.findOne({ office_name: 'ADMISSIONS' })
+  const result = Faqs.findOne({ office_name: officeName })
   const faq = await result
 
   res.status(200).send({ value: faq })
 
-  // const result = User.findOne({ office_name: 'SDAO' })
-  // const user = await result
-
-  // res.status(200).send({ value: user })
 }
 
-const getUser = async (req, res) => {
-  const result = User.findOne({ name: 'testuser' })
-  const user = await result
 
-  res.status(200).send({ value: user })
-}
 
 export { getQuestionsFromOffice }
