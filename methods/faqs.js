8d5af615bf965633056afeb4ee132ffe2132b63d
Abigail Capio <sammarco1022@gmail.com>
2022-03-03 15:46:44 +0800

    Add changes

--- methods/faqs.js
+++ methods/faqs.js
@@ -4,7 +4,7 @@ import User from '../models/user.js'
 const getQuestionsFromOffice = async (req, res) => {
   const { officeName } = req.body
 
-  const result = Faqs.findOne({ office_name: officeName })
+  const result = Faqs.findOne({ office_name: officeName }, {'_id': false})
   const faq = await result
 
   res.status(200).send({ value: faq })
