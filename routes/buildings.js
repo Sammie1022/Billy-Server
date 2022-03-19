c07384ac868c0e8c7c1ba9f1bcc468ec1f321fb1
Fhillip Bagsic <fhillipbagsic@Fhillips-MacBook-Pro.local>
2022-03-19 13:09:31 +0800

    Add admin functionalities

--- routes/buildings.js
+++ routes/buildings.js
@@ -1,8 +1,10 @@
 import express from 'express'
-import { getBuilding } from '../methods/buildings.js'
+import { getBuilding, getBuildings } from '../methods/buildings.js'
 
 const router = express.Router()
 
 router.route('/getbuilding').post(getBuilding)
+
+router.route('/getbuildings').post(getBuildings)
 
 export default router
